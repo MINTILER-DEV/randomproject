b8e965279c2c71411bb35f90560a91fca63e9ec9
Antonio <64821480+MINTILER-DEV@users.noreply.github.com>
2023-10-22 11:04:00 -0500

    finished OrcaHub script

--- Scripts/Hubs/OrcaHub.lua
+++ Scripts/Hubs/OrcaHub.lua
@@ -1,3 +1,6 @@
 -- Orca Hub Script
 print("Orca hub executed")
-print("Script by AntonioOzurumba. (MINTILER on youtube)")
+print("Script hub by AntonioOzurumba. (MINTILER on youtube)")
+print("Also thank you for downloading my script hub on github!!!")
+
+loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/richie0866/orca/master/public/latest.lua"))()
