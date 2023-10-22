b6217dda110d12fda40d784cecf088c2b8ce661c
Antonio <64821480+MINTILER-DEV@users.noreply.github.com>
2023-10-22 11:22:38 -0500

    Delete Scripts/Hubs/OrcaHub.lua

--- Scripts/Hubs/OrcaHub.lua
+++
@@ -1,6 +0,0 @@
--- Orca Hub Script
-print("Orca hub executed")
-print("Script hub by AntonioOzurumba. (MINTILER on youtube)")
-print("Also thank you for downloading my script hub on github!!!")
-
-loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/richie0866/orca/master/public/latest.lua"))()
